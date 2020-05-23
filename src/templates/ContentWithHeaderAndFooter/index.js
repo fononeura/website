e3fe41b28c6a4368610ba2074f65aa22311e0a7c
Vagner Clementino <vagner.clementino@gmail.com>
2020-05-23 20:42:34 -0300

    Refactoring the content component

--- src/templates/ContentWithHeaderAndFooter/index.js
+++ src/templates/ContentWithHeaderAndFooter/index.js
@@ -1,35 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import {Layout} from 'antd'
 import style from './style.module.css';
 import logo from '../../assets/icone-sem-fundo.png';
 
-const { Header, Content, Footer } = Layout;
-
-const ContentWithHeaderAndFooter = ({children, size}) =>{
+const ContentWithHeaderAndFooter = ({children}) =>{
 return (
-        <Layout className={style.content}>
-            <Header className={style.header}>
-                <div className={style.logo}>
-                    <img src={logo} alt="Fononeura logo. Retorna para página principal"/>
-                </div>
-            </Header>
-            <Content className={style.content}>
-                    {children}
-            </Content>
-            <Footer className={style.footer}>
-                {`©${new Date().getFullYear()} Fononeura`}
-            </Footer>
-        </Layout>
+    <div className={style.content}>
+        <header className={style.header}>
+            <div className={style.logo}>
+                <img src={logo} alt="Fononeura logo. Retorna para página principal"/>
+            </div>
+        </header>
+        <main className={style.main}>
+            {children}
+        </main>
+        <footer className={style.footer}>
+            {`©${new Date().getFullYear()} Fononeura`}
+        </footer>
+    </div>
     );
 };
 
 ContentWithHeaderAndFooter.propTypes = {
     children: PropTypes.any,
-    size: PropTypes.oneOf(['medium', 'large']),
 };
 
-ContentWithHeaderAndFooter.defaultProps = {
-    size: 'large',
-  };
 export default ContentWithHeaderAndFooter;
