781252a1593de85a87785fdba32544d723157a52
Vagner Clementino <vagner.clementino@gmail.com>
2020-05-31 20:49:11 -0300

    Creates card component

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/sobre" component={Home} />
+    <Route exact path="/sobre" component={About} />
   </Switch>
 );
 
